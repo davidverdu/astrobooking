4fecb1492aa9cbc673066a3150bb15108eb76206
Josué Hernández <jhernandez@inerza.com>
2025-12-02 19:10:05 +0000

    rename method getRockets in rocketService

--- src/main/java/com/astrobookings/domain/RocketService.java
+++ src/main/java/com/astrobookings/domain/RocketService.java
@@ -12,7 +12,7 @@ public class RocketService {
         this.rocketRepository = rocketRepository;
     }
 
-    public List<Rocket> getRocketss() {
+    public List<Rocket> getRockets() {
         return rocketRepository.findAll();
     }
 
